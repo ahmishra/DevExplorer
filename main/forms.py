c0f18884b52139a2d0cb96b65522a35f0242704e
ahmishra <umasaryan@gmail.com>
2021-06-10 12:13:21 +0530

    Added Forms

---
+++ main/forms.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms.fields.core import BooleanField, StringField
+from wtforms.fields.simple import PasswordField, SubmitField,
+from wtforms.validators import DataRequired, EqualTo, Length
+
+class RegistrationForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired(), Length(min=3, max=20)])
+    password = PasswordField("Password", validators=[
+                             DataRequired(), Length(min=8, max=64)])
+    password_confirm = PasswordField("Confirm Password", validators=[
+                             DataRequired(), Length(min=8, max=64), EqualTo('password')])
+
+    submit = SubmitField('Register')
+
+
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[
+                           DataRequired(), Length(min=3, max=20)])
+    password = PasswordField("Password", validators=[
+                             DataRequired(), Length(min=8, max=64)])
+    remember_me = BooleanField("Remember Me")
+    submit = SubmitField('Login')
--- main/routes.py
+++ main/routes.py
@@ -1,7 +1,19 @@
+from re import LOCALE
 from flask import request, render_template, redirect, url_for, flash
 from main import app
+from main.forms import RegistrationForm, LoginForm
 
 @app.route("/")
 def home():
 	return render_template("index.html")
 
+@app.route("/register")
+def register():
+	form = RegistrationForm()
+	return render_template("register.html", form=form)
+
+
+@app.route("/login")
+def login():
+	form = LoginForm()
+	return render_template("login.html", form=form)

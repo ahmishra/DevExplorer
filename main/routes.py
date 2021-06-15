4bc5677c901bc65e87870ee38ad59aca149ac98f
ahmishra <umasaryan@gmail.com>
2021-06-15 12:07:05 +0530

    bug fix

--- main/routes.py
+++ main/routes.py
@@ -7,6 +7,9 @@ from main import app, bcrypt, db, ScrapeNews
 import xlrd
 import os
 
+xlrd.xlsx.ensure_elementtree_imported(False, None)
+xlrd.xlsx.Element_has_iter = True
+
 
 # Home Page
 @app.route("/")
